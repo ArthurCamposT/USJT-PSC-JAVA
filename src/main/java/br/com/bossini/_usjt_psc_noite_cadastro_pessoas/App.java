ffba17452169626cddfcbd1fad0ec0b92115d8d1
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-12 17:43:46 -0300

    apostila 10: operacao de atualizacao e menu de opcoes implementados

--- src/main/java/br/com/bossini/_usjt_psc_noite_cadastro_pessoas/App.java
+++ src/main/java/br/com/bossini/_usjt_psc_noite_cadastro_pessoas/App.java
@@ -7,19 +7,42 @@ package br.com.bossini._usjt_psc_noite_cadastro_pessoas;
 
 import javax.swing.JOptionPane;
 
+
 public class App {
     public static void main(String[] args) {
-        //pegar o nome, fone e e-mail de uma pessoa junto ao usuário
-        //construir um objeto do tipo Pessoa com esses dados
-        //fazer o mapeamento objeto relacional para que esse objeto seja armazenado na base
-        String nome = JOptionPane.showInputDialog("Digite o nome da pessoa");
-        String fone = JOptionPane.showInputDialog("Digite o fone da pessoa");
-        String email = JOptionPane.showInputDialog("Digite o email da pessoa");
-        
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome(nome);
-        pessoa.setFone(fone);
-        pessoa.setEmail(email);
+        //1. Cadastrar 2. Atualizar 3. Apagar 4. Listar pessoas 0. Sair
+        String menu = "1-Cadastrar\n2-Atualizar\n3-Apagar\n4-Listar\n0-Sair";
+        int op;
+        do{
+            op = Integer.parseInt(JOptionPane.showInputDialog(menu));
+            switch(op){
+                case 1:
+                    
+                    break;
+                case 2:
+                    int codigo = Integer.parseInt(JOptionPane.showInputDialog("Código?"));
+                    String nome = JOptionPane.showInputDialog("Nome?");
+                    String fone = JOptionPane.showInputDialog("Fone?");
+                    String email = JOptionPane.showInputDialog("E-mail?");
+                    Pessoa p = new Pessoa(codigo, nome, fone, email);
+                    p.atualizar();
+                    JOptionPane.showMessageDialog(null, "OK, atualizou!");
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 0:
+                    
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida");
+            }
+            
+        }while (op != 0);
     }
 }
 
@@ -29,6 +52,19 @@ public class App {
 
 
 
+        //pegar o nome, fone e e-mail de uma pessoa junto ao usuário
+        //construir um objeto do tipo Pessoa com esses dados
+        //fazer o mapeamento objeto relacional para que esse objeto seja armazenado na base
+//        String nome = JOptionPane.showInputDialog("Digite o nome da pessoa");
+//        String fone = JOptionPane.showInputDialog("Digite o fone da pessoa");
+//        String email = JOptionPane.showInputDialog("Digite o email da pessoa");
+//        
+//        Pessoa pessoa = new Pessoa();
+//        pessoa.setNome(nome);
+//        pessoa.setFone(fone);
+//        pessoa.setEmail(email);
+//        
+//        pessoa.inserir();
 //        Connection conexao = fabricaConexoes.obterConexao();
 //        ConnectionFactory fabricaConexoes = new ConnectionFactory();
 //        System.out.println(conexao);
--- src/main/java/br/com/bossini/_usjt_psc_noite_cadastro_pessoas/Pessoa.java
+++ src/main/java/br/com/bossini/_usjt_psc_noite_cadastro_pessoas/Pessoa.java
@@ -4,6 +4,8 @@
  */
 package br.com.bossini._usjt_psc_noite_cadastro_pessoas;
 
+import java.sql.*;
+
 /**
  *
  * @author rodrigo
@@ -20,6 +22,65 @@ public class Pessoa {
     
     //email VARCHAR(200)
     private String email;
+    
+    Pessoa (int codigo, String nome, String fone, String email){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.fone = fone;
+        this.email = email;
+    }
+    
+    public void atualizar(){
+        try{        
+            //1. Especificar o comando SQL
+            String sql = "UPDATE tb_pessoa SET nome = ?, fone = ?, email = ? WHERE cod_pessoa = ?";
+            //2. Abrir uma conexão com o MySQL
+            Connection conexao = new ConnectionFactory().obterConexao();
+            //3. Preparar o comando
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            //4. Substituir os placeholders
+            ps.setString(1, nome);
+            ps.setString(2, fone);
+            ps.setString(3, email);
+            ps.setInt(4, codigo);        
+            //5. Executar o comando
+            ps.execute();
+            //6. Fechar o comando e a conexão
+            ps.close();
+            conexao.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+    //cláusula catch or declare    
+    public void inserir(){
+        try{        
+            //não faça isso!
+            //String sql = "INSERT INTO tb_pessoa(nome, fone, email) VALUES( '" + nome + "', " + fone + ", '" + email + "')";
+            //1. Especificar o comando SQL
+            String sql = "INSERT INTO tb_pessoa(nome, fone, email) VALUES (?, ?, ?)";
+            //2. Abrir uma conexão com o MySQL
+            ConnectionFactory conexaoFactory = new ConnectionFactory();
+            Connection conexao = conexaoFactory.obterConexao();
+            //3. Pré compilar o comando
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            //4. Preencher os dados faltantes (substituir os placeholders)
+            ps.setString(1, nome);
+            ps.setString(2, fone);
+            ps.setString(3, email);        
+            //5. Executar o comando
+            ps.execute();        
+            //6. Fechar a conexão
+            ps.close();
+            conexao.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
     public int getCodigo() {
         return codigo;
